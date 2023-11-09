7640c66a5e9d30e7f477e251b4c326daae352e10
Alnfonseca <afonsecadesantana@gmail.com>
2023-11-09 17:19:22 -0300

    Avaliação

--- LStreetwear/LStreetwear/frmAvaliacao.Designer.cs
+++ LStreetwear/LStreetwear/frmAvaliacao.Designer.cs
@@ -32,17 +32,22 @@ namespace LStreetwear
             this.lblNomeCliente = new System.Windows.Forms.Label();
             this.lblNomeFunc = new System.Windows.Forms.Label();
             this.lblQualidadeAtend = new System.Windows.Forms.Label();
-            this.lblValorComissao = new System.Windows.Forms.Label();
             this.txtNomeCliente = new System.Windows.Forms.TextBox();
             this.txtNomeFunc = new System.Windows.Forms.TextBox();
-            this.txtValorComissao = new System.Windows.Forms.TextBox();
             this.cbbQualidadeAtend = new System.Windows.Forms.ComboBox();
             this.btnCalcularComiss = new System.Windows.Forms.Button();
             this.btnVoltar = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpDataCompra = new System.Windows.Forms.DateTimePicker();
             this.lblDataCompra = new System.Windows.Forms.Label();
             this.txtValorTotal = new System.Windows.Forms.TextBox();
             this.lblValorTotal = new System.Windows.Forms.Label();
+            this.txtValorComissao = new System.Windows.Forms.TextBox();
+            this.lblValorComissao = new System.Windows.Forms.Label();
+            this.txtValorCompra = new System.Windows.Forms.TextBox();
+            this.lblValorCompra = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtcodFunc = new System.Windows.Forms.TextBox();
+            this.lblcodFunc = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblNomeCliente
@@ -69,21 +74,11 @@ namespace LStreetwear
             // 
             this.lblQualidadeAtend.AutoSize = true;
             this.lblQualidadeAtend.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQualidadeAtend.Location = new System.Drawing.Point(134, 229);
+            this.lblQualidadeAtend.Location = new System.Drawing.Point(134, 236);
             this.lblQualidadeAtend.Name = "lblQualidadeAtend";
             this.lblQualidadeAtend.Size = new System.Drawing.Size(181, 18);
             this.lblQualidadeAtend.TabIndex = 2;
             this.lblQualidadeAtend.Text = "Qualidade do Atendimento";
-            // 
-            // lblValorComissao
-            // 
-            this.lblValorComissao.AutoSize = true;
-            this.lblValorComissao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblValorComissao.Location = new System.Drawing.Point(180, 332);
-            this.lblValorComissao.Name = "lblValorComissao";
-            this.lblValorComissao.Size = new System.Drawing.Size(135, 18);
-            this.lblValorComissao.TabIndex = 3;
-            this.lblValorComissao.Text = "Valor da Comissão";
             // 
             // txtNomeCliente
             // 
@@ -99,13 +94,6 @@ namespace LStreetwear
             this.txtNomeFunc.Size = new System.Drawing.Size(422, 20);
             this.txtNomeFunc.TabIndex = 2;
             // 
-            // txtValorComissao
-            // 
-            this.txtValorComissao.Location = new System.Drawing.Point(367, 337);
-            this.txtValorComissao.Name = "txtValorComissao";
-            this.txtValorComissao.Size = new System.Drawing.Size(209, 20);
-            this.txtValorComissao.TabIndex = 5;
-            // 
             // cbbQualidadeAtend
             // 
             this.cbbQualidadeAtend.FormattingEnabled = true;
@@ -115,14 +103,14 @@ namespace LStreetwear
             "Ótimo - 8%",
             "Bom - 5%",
             "Ruim - 2%"});
-            this.cbbQualidadeAtend.Location = new System.Drawing.Point(367, 226);
+            this.cbbQualidadeAtend.Location = new System.Drawing.Point(364, 233);
             this.cbbQualidadeAtend.Name = "cbbQualidadeAtend";
             this.cbbQualidadeAtend.Size = new System.Drawing.Size(209, 21);
             this.cbbQualidadeAtend.TabIndex = 4;
             // 
             // btnCalcularComiss
             // 
-            this.btnCalcularComiss.Location = new System.Drawing.Point(367, 373);
+            this.btnCalcularComiss.Location = new System.Drawing.Point(156, 373);
             this.btnCalcularComiss.Name = "btnCalcularComiss";
             this.btnCalcularComiss.Size = new System.Drawing.Size(206, 54);
             this.btnCalcularComiss.TabIndex = 7;
@@ -132,7 +120,7 @@ namespace LStreetwear
             // 
             // btnVoltar
             // 
-            this.btnVoltar.Location = new System.Drawing.Point(665, 387);
+            this.btnVoltar.Location = new System.Drawing.Point(670, 387);
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(111, 40);
             this.btnVoltar.TabIndex = 8;
@@ -140,19 +128,19 @@ namespace LStreetwear
             this.btnVoltar.UseVisualStyleBackColor = true;
             this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
             // 
-            // dateTimePicker1
+            // dtpDataCompra
             // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(294, 131);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(80, 20);
-            this.dateTimePicker1.TabIndex = 3;
+            this.dtpDataCompra.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataCompra.Location = new System.Drawing.Point(621, 134);
+            this.dtpDataCompra.Name = "dtpDataCompra";
+            this.dtpDataCompra.Size = new System.Drawing.Size(95, 20);
+            this.dtpDataCompra.TabIndex = 3;
             // 
             // lblDataCompra
             // 
             this.lblDataCompra.AutoSize = true;
             this.lblDataCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDataCompra.Location = new System.Drawing.Point(107, 131);
+            this.lblDataCompra.Location = new System.Drawing.Point(459, 135);
             this.lblDataCompra.Name = "lblDataCompra";
             this.lblDataCompra.Size = new System.Drawing.Size(117, 18);
             this.lblDataCompra.TabIndex = 19;
@@ -160,7 +148,7 @@ namespace LStreetwear
             // 
             // txtValorTotal
             // 
-            this.txtValorTotal.Location = new System.Drawing.Point(367, 283);
+            this.txtValorTotal.Location = new System.Drawing.Point(364, 332);
             this.txtValorTotal.Name = "txtValorTotal";
             this.txtValorTotal.Size = new System.Drawing.Size(209, 20);
             this.txtValorTotal.TabIndex = 21;
@@ -169,32 +157,99 @@ namespace LStreetwear
             // 
             this.lblValorTotal.AutoSize = true;
             this.lblValorTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblValorTotal.Location = new System.Drawing.Point(180, 278);
+            this.lblValorTotal.Location = new System.Drawing.Point(180, 327);
             this.lblValorTotal.Name = "lblValorTotal";
             this.lblValorTotal.Size = new System.Drawing.Size(79, 18);
             this.lblValorTotal.TabIndex = 20;
             this.lblValorTotal.Text = "Valor Total";
+            // 
+            // txtValorComissao
+            // 
+            this.txtValorComissao.Location = new System.Drawing.Point(364, 283);
+            this.txtValorComissao.Name = "txtValorComissao";
+            this.txtValorComissao.Size = new System.Drawing.Size(209, 20);
+            this.txtValorComissao.TabIndex = 23;
+            // 
+            // lblValorComissao
+            // 
+            this.lblValorComissao.AutoSize = true;
+            this.lblValorComissao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorComissao.Location = new System.Drawing.Point(180, 278);
+            this.lblValorComissao.Name = "lblValorComissao";
+            this.lblValorComissao.Size = new System.Drawing.Size(135, 18);
+            this.lblValorComissao.TabIndex = 22;
+            this.lblValorComissao.Text = "Valor da Comissão";
+            // 
+            // txtValorCompra
+            // 
+            this.txtValorCompra.Location = new System.Drawing.Point(364, 191);
+            this.txtValorCompra.Name = "txtValorCompra";
+            this.txtValorCompra.Size = new System.Drawing.Size(212, 20);
+            this.txtValorCompra.TabIndex = 25;
+            // 
+            // lblValorCompra
+            // 
+            this.lblValorCompra.AutoSize = true;
+            this.lblValorCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorCompra.Location = new System.Drawing.Point(177, 186);
+            this.lblValorCompra.Name = "lblValorCompra";
+            this.lblValorCompra.Size = new System.Drawing.Size(120, 18);
+            this.lblValorCompra.TabIndex = 24;
+            this.lblValorCompra.Text = "Valor da Compra";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(412, 373);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(204, 54);
+            this.btnLimpar.TabIndex = 26;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtcodFunc
+            // 
+            this.txtcodFunc.Location = new System.Drawing.Point(294, 137);
+            this.txtcodFunc.Name = "txtcodFunc";
+            this.txtcodFunc.Size = new System.Drawing.Size(68, 20);
+            this.txtcodFunc.TabIndex = 28;
+            // 
+            // lblcodFunc
+            // 
+            this.lblcodFunc.AutoSize = true;
+            this.lblcodFunc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcodFunc.Location = new System.Drawing.Point(107, 137);
+            this.lblcodFunc.Name = "lblcodFunc";
+            this.lblcodFunc.Size = new System.Drawing.Size(159, 18);
+            this.lblcodFunc.TabIndex = 27;
+            this.lblcodFunc.Text = "Código do Funcionário";
             // 
             // frmAvaliacao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtcodFunc);
+            this.Controls.Add(this.lblcodFunc);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtValorCompra);
+            this.Controls.Add(this.lblValorCompra);
+            this.Controls.Add(this.txtValorComissao);
+            this.Controls.Add(this.lblValorComissao);
             this.Controls.Add(this.txtValorTotal);
             this.Controls.Add(this.lblValorTotal);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dtpDataCompra);
             this.Controls.Add(this.lblDataCompra);
             this.Controls.Add(this.btnVoltar);
             this.Controls.Add(this.btnCalcularComiss);
             this.Controls.Add(this.cbbQualidadeAtend);
-            this.Controls.Add(this.txtValorComissao);
             this.Controls.Add(this.txtNomeFunc);
             this.Controls.Add(this.txtNomeCliente);
-            this.Controls.Add(this.lblValorComissao);
             this.Controls.Add(this.lblQualidadeAtend);
             this.Controls.Add(this.lblNomeFunc);
             this.Controls.Add(this.lblNomeCliente);
             this.Name = "frmAvaliacao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LStreetwear - Avaliação do Cliente";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -206,16 +261,21 @@ namespace LStreetwear
         private System.Windows.Forms.Label lblNomeCliente;
         private System.Windows.Forms.Label lblNomeFunc;
         private System.Windows.Forms.Label lblQualidadeAtend;
-        private System.Windows.Forms.Label lblValorComissao;
         private System.Windows.Forms.TextBox txtNomeCliente;
         private System.Windows.Forms.TextBox txtNomeFunc;
-        private System.Windows.Forms.TextBox txtValorComissao;
         private System.Windows.Forms.ComboBox cbbQualidadeAtend;
         private System.Windows.Forms.Button btnCalcularComiss;
         private System.Windows.Forms.Button btnVoltar;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpDataCompra;
         private System.Windows.Forms.Label lblDataCompra;
         private System.Windows.Forms.TextBox txtValorTotal;
         private System.Windows.Forms.Label lblValorTotal;
+        private System.Windows.Forms.TextBox txtValorComissao;
+        private System.Windows.Forms.Label lblValorComissao;
+        private System.Windows.Forms.TextBox txtValorCompra;
+        private System.Windows.Forms.Label lblValorCompra;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtcodFunc;
+        private System.Windows.Forms.Label lblcodFunc;
     }
 }
--- LStreetwear/LStreetwear/frmAvaliacao.cs
+++ LStreetwear/LStreetwear/frmAvaliacao.cs
@@ -16,6 +16,40 @@ namespace LStreetwear
         public frmAvaliacao()
         {
             InitializeComponent();
+            desabilitarInicial();
+        }
+        public void desabilitarInicial()
+        {
+            txtValorComissao.Enabled = false;
+        }
+        public void limparTudo()
+        {
+            txtNomeCliente.Clear();
+            txtNomeFunc.Clear();
+            txtValorCompra.Clear();
+            txtValorComissao.Clear();
+            txtValorTotal.Clear();
+        }
+        public int  avaliacao(int codigo)
+        {
+            MySqlCommand comm = new MySqlCommand();
+            comm.CommandText = "insert into tbAvaliacoes (codFunc, nome, dataCompra, qualidadeAtend, valorComiss, valortotal) values (@codFunc, @nome, @dataCompra, @qualidadeAtend, @valorComiss, @valortotal);";
+            comm.CommandType = CommandType.Text;
+
+            comm.Parameters.Clear();
+            comm.Parameters.Add("@codFunc",MySqlDbType.Int32).Value = codigo;
+            comm.Parameters.Add("@nome",MySqlDbType.VarChar, 100).Value = txtNomeFunc.Text;
+            comm.Parameters.Add("@dataCompra", MySqlDbType.Date).Value = Convert.ToDateTime(dtpDataCompra.Text);
+            comm.Parameters.Add("@qualidadeAtend",MySqlDbType.VarChar, 10).Value = cbbQualidadeAtend.Text;
+            comm.Parameters.Add("@valorComiss",MySqlDbType.Int32).Value = txtValorComissao.Text;
+            comm.Parameters.Add("@valorTotal",MySqlDbType.Int32).Value = txtValorTotal.Text;
+
+
+            comm.Connection = Conexao.conectar();
+
+            int res = comm.ExecuteNonQuery();
+            Conexao.desconectar();
+            return res;
         }
 
         private void btnVoltar_Click(object sender, EventArgs e)
@@ -32,28 +66,51 @@ namespace LStreetwear
         //calculando comissão
         public void calcularComissao()
         {
-            double comissao, total;
+            double total = 0,valor = 0, comissao = 0;
+            int indice = 0;
 
             try
             {
-                comissao = Convert.ToDouble(txtValorComissao);
-                total = Convert.ToDouble(txtValorTotal);
+                comissao = Convert.ToDouble(cbbQualidadeAtend.SelectedIndex);
+                valor = Convert.ToDouble(txtValorCompra.Text);
 
-                if(cbbQualidadeAtend.SelectedItem == "Excelente - 10%")
+               if(indice == 0)
                 {
-                    comissao = total*0.10/100;
-                   
+                    comissao = valor * 10 / 100;
+                    total = comissao + valor;
                 }
-
+                if (indice == 1)
+                {
+                    comissao = valor * 8 / 100;
+                    total = comissao + valor;
+                }
+                if(indice == 2)
+                {
+                    comissao = valor * 5 / 100;
+                    total = comissao + valor;
+                }
+                if(indice == 3)
+                {
+                    comissao = valor * 2 / 100;
+                    total = comissao + valor;
+                }
+                txtValorTotal.Text = total.ToString();
+                txtValorComissao.Text = comissao.ToString();
+                
             }
             catch (Exception)
             {
 
                MessageBox.Show("conta incorreta!", "mensagem do sistema");
             }
-            // arrumar
+            
             
           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            limparTudo();
         }
     }
 }
